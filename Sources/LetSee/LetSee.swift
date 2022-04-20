5d4a9f7821b5e38db080808a5a879039d1c480b6
Farshad jahanmanesh <Farshad.jahanmanesh@metatude.org>
2022-04-20 23:26:26 +0430

    - fixes some bugs, adds support for Cocoapods

--- Sources/LetSee/LetSee.swift
+++ Sources/LetSee/LetSee.swift
@@ -3,8 +3,9 @@ final public class LetSee {
     public init(_ baseUrl: String = "") {
         self.webServer = .init(apiBaseUrl: baseUrl)
     }
-    
+#if canImport(Moya)
     public var logger: LetSeeLogs {
         LetSeeLogs(webServer: webServer)
     }
+#endif
 }
