f8b7cd8db17ab06195870fec91bfd10913d40411
Farshad jahanmanesh <Farshadjahanmanesh@gmail.com>
2023-01-15 15:19:44 +0100

    feat: adds mocks

--- Example/CatAPIProject/CatAPIProject/CatAPIProjectApp.swift
+++ Example/CatAPIProject/CatAPIProject/CatAPIProjectApp.swift
@@ -10,31 +10,42 @@ import LetSee
 import LetSeeInAppView
 @main
 struct CatAPIProjectApp: App {
-	@State var letSeeWindow: UIWindow?
+	@State var letSeeWindow: UIWindow? {
+		didSet {
+			LetSee.shared.config(LetSee.Configuration.init(baseURL: URL(string: "https://api.thecatapi.com/")!,
+														   isMockEnabled: false,
+														   shouldCutBaseURLFromURLsTitle: true))
+			LetSee.shared.addMocks(from: Bundle.main.bundlePath + "/Mocks/Mocks")
+			LetSee.shared.addScenarios(from: Bundle.main.bundlePath + "/Mocks/Scenarios")
+			LetSee.shared.interceptor.liveToServer = { request, completion in
+				URLSession.shared.dataTask(with: request, completionHandler: { data, res, err in
+					completion?(res, data, err)
+				})
+				.resume()
+			}
+		}
+	}
 	var window: UIWindow? {
-		   guard let scene = UIApplication.shared.connectedScenes.first,
-				 let windowSceneDelegate = scene.delegate as? UIWindowSceneDelegate,
-				 let window = windowSceneDelegate.window else {
-			   return nil
-		   }
-		let letSeeWindow = LetSeeWindow(frame: window!.frame)
-		letSeeWindow.windowScene = window!.windowScene
-		self.letSeeWindow = letSeeWindow
-		LetSee.shared.config(LetSee.Configuration.init(baseURL: URL(string: "https://api.thecatapi.com/")!, isMockEnabled: false, shouldCutBaseURLFromURLsTitle: true))
-		LetSee.shared.addMocks(from: Bundle.main.bundlePath + "/Mocks")
-		LetSee.shared.addScenarios(from: Bundle.main.bundlePath + "/Scenarios")
+		guard let scene = UIApplication.shared.connectedScenes.first,
+			  let windowSceneDelegate = scene.delegate as? UIWindowSceneDelegate,
+			  let window = windowSceneDelegate.window else {
+			return nil
+		}
 		return window
 	}
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
 				.onAppear {
 					guard let window = window else {
 						return
 					}
+					let letSeeWindow = LetSeeWindow(frame: window.frame)
+					letSeeWindow.windowScene = window.windowScene
+					self.letSeeWindow = letSeeWindow
 				}
-        }
+		}
 
-    }
+	}
 }
 
